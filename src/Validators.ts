85ae350d87155a764814099da8a99c7d8249c4f9
Universal Studio <10200399+TMUniversal@users.noreply.github.com>
2022-02-14 13:52:41 +0100

    refactor(validators): clean up validators
    used to be a class for some reason
    is now a set of functions

--- src/Validators.ts
+++ src/Validators.ts
@@ -1,50 +1,52 @@
 /* eslint-disable no-useless-escape */
 
 /**
- * Validators for various strings
+ * RegExp for URL validation
  */
-class Validators {
-  /**
-   * RegExp for URL validation
-   */
-  public urlRegex: RegExp = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{1,24}\/([-a-zA-Z0-9()@:%._\+~#?&\/=]*)$/m
-  /**
-   * RegExp for Discord emoji name validation
-   */
-  public emojiRegex: RegExp = /^[\w]{2,32}$/m
-  private readonly invalidName: string = 'invalid_name'
+export const URL_REGEX: RegExp = /^https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-z]{1,24}\/([-a-zA-Z0-9()@:%._\+~#?&\/=]*)$/m
+/**
+ * RegExp for Discord emoji name validation
+ */
+export const EMOJI_REGEX: RegExp = /^[\w]{2,32}$/m
+export const INVALID_EMOJI_NAME: string = 'invalid_name'
 
-  /**
-   * Validate a URL
-   * @param {String} url URL to validate
-   * @returns {Boolean} wether the given string is a valid url
-   */
-  public isUrl (url: string): boolean {
-    return this.urlRegex.test(url ?? '')
-  }
-
-  /**
-   * Validate an emoji name
-   * @param {String} name name to validate
-   * @returns {Boolean} wether the given string is a valid name for discord emojis
-   */
-  public isValidEmojiName (name: string): boolean {
-    return this.emojiRegex.test(name ?? '')
-  }
-
-  /**
-   * Get a name that's valid for discord emojis
-   * @param {String} name name to try
-   * @returns {String | "invalid_name"} The filtered name or a substitute
-   */
-  public getValidEmojiName (name: string): string {
-    if (!name) return this.invalidName
-    // Filter disallowed characters
-    const emojiName = name.replace(/[^\w]/gi, '')
-    if (!emojiName) return this.invalidName
-    else if (!this.isValidEmojiName(emojiName)) return this.invalidName
-    return emojiName
-  }
+/**
+ * Validate a URL
+ * @param {String} url URL to validate
+ * @returns {Boolean} wether the given string is a valid url
+ */
+export function isUrl (url: string): boolean {
+  return URL_REGEX.test(url ?? '')
 }
 
-export default new Validators()
+/**
+ * Validate an emoji name
+ * @param {String} name name to validate
+ * @returns {Boolean} wether the given string is a valid name for discord emojis
+ */
+export function isValidEmojiName (name: string): boolean {
+  return EMOJI_REGEX.test(name ?? '')
+}
+
+/**
+ * Get a name that's valid for discord emojis
+ * @param {String} name name to try
+ * @returns {String | "invalid_name"} The filtered name or a substitute
+ */
+export function getValidEmojiName (name: string): string {
+  if (!name) return INVALID_EMOJI_NAME
+  // Filter disallowed characters
+  const emojiName = name.replace(/[^\w]/gi, '')
+  if (!emojiName) return INVALID_EMOJI_NAME
+  else if (!isValidEmojiName(emojiName)) return INVALID_EMOJI_NAME
+  return emojiName
+}
+
+export default {
+  isValidEmojiName,
+  getValidEmojiName,
+  isUrl,
+  invalidName: INVALID_EMOJI_NAME,
+  emojiRegex: EMOJI_REGEX,
+  urlRegex: URL_REGEX
+}
